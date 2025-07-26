c6f7ddb347cb697a8d8a418a9702baf39a82df78
Panchami Hegde <panchhegde2004@gmail.com>
2025-07-26 22:23:45 +0530

    Added README and made final updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const {authUser,loading}=useContext(AuthContext)
   if (loading) return <div className="text-white p-4">Loading...</div>
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
